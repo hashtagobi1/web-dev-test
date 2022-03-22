ab26033e76c1fb2597e1f078190ad4b99a89e3ff
obi <anokwuruobi@gmail.com>
2022-03-22 20:47:18 +0000

    2nd commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,9 +23,20 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <a 
+          
+          
+          
+          href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information 
+            <p>Find in-d
+              
+              
+              
+              
+              
+              
+              epth information 
               
               
               about Next.js features and API.</p>

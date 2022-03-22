6844eb11137c93e9ec04aa81a7a800aa23619e27
obi <anokwuruobi@gmail.com>
2022-03-22 21:09:28 +0000

    once more?

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,10 +22,8 @@ const Home: NextPage = () => {
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
-        <div
-        className={styles.grid}>
-          <a 
-          href="https://nextjs.org/docs" className={styles.card}>
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-d epth information about Next.js features and API.</p>
           </a>
